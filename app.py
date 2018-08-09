da9487b8ca64f778f6f3ca3e55ecd3f7232f9c8f
Jason Dickey <jd.glenn@ymail.com>
2018-08-08 18:36:08 -0700

    final tableau

--- app.py
+++ app.py
@@ -10,7 +10,20 @@ app = Flask(__name__, static_url_path='')
 @app.route("/")
 def index():
     """Return the homepage."""
-    return render_template('index.html')
+    return render_template('page2.html')
+
+
+@app.route('/names')
+def names():
+    """Return a list of sample names."""
+
+    df_train = pd.read_json('train.json')
+    df_cuisines = df_train['cuisine'].unique()
+
+    df_cuisines.set_index('otu_id', inplace=True)
+
+    # Return a list of the column names (sample names)
+    return jsonify(list(df_cuisines))
 
 
 if __name__ == "__main__":
